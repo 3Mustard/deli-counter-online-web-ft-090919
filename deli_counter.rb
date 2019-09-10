d9f23700d6c9f3ea86bc00d81a9dd5bcacc71c98
Matthew C <m23.cullen@gmail.com>
2019-09-10 17:06:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 def line(array)
-  current_line = "The line is currently: "
+  current_line = []
   if line.size > 0
     array.each_with_index do |name,index|
-      
+      index += 1
+      current_line << "#{index}. #{name}"
+    end
+    puts "The line is currently: " + current_line.join
 end
